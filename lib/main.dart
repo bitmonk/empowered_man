d15f44819de9ffedc82f490806faaad1012e6223
Parbat Tamang <parbattamang1234@gmail.com>
2025-04-02 18:01:13 +0545

    added flavour(api)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+
 import 'package:empowered/core/build_variants/environment_entry_points.dart';
-import 'package:empowered/core/extension/extensions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'app.dart';
+import 'flavors.dart';
 
 void main() async {
+  F.appFlavor = Flavor.values.firstWhere(
+    (element) => element.name == appFlavor,
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
