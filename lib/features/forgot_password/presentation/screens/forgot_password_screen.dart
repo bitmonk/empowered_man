4c7e1d7fdda623509e64242243504914d3d6d686
lakshydeep-14 <lvikramsah@gmail.com>
2025-04-23 10:02:02 +0545

    obx added

--- lib/features/forgot_password/presentation/screens/forgot_password_screen.dart
+++ lib/features/forgot_password/presentation/screens/forgot_password_screen.dart
@@ -9,50 +9,52 @@ class ForgotPasswordScreen extends GetView<ForgotPasswordController> {
   Widget build(BuildContext context) {
     return AppScaffold(
       appBar: const CustomAppBar(),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Center(child: AppLogo()),
-                const VerticalSpacing(32),
-                const Text(
-                  'Forgot password?',
-                  style: AppTextStyles.titleSm,
-                ),
-                const VerticalSpacing(16),
-                const Text(
-                  'Please enter the email associated with your account.',
-                  style: TextStyle(color: AppColors.neutral600),
-                ),
-                const VerticalSpacing(32),
-                Form(
-                  key: controller.formKey,
-                  child: AppTextFormField(
-                    controller: controller.emailController,
-                    validator: ValidationBuilder().email().build(),
-                    labelText: 'Email Address',
-                    textInputType: TextInputType.emailAddress,
+      body: Obx(
+        () => SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Center(child: AppLogo()),
+                  const VerticalSpacing(32),
+                  const Text(
+                    'Forgot password?',
+                    style: AppTextStyles.titleSm,
                   ),
-                ),
-                const VerticalSpacing(32),
-                AppOutlinedButton(
-                  text: 'Continue',
-                  isLoading:
-                      controller.forgotPasswordState.value == TheStates.loading,
-                  onPressed: () async {
-                    if (!AppUtils.validateForm(controller.formKey)) {
-                      return;
-                    }
-                    if (await controller.forgotPassword()) {
-                      Navigator.pop(context);
-                    }
-                  },
-                ),
-                const VerticalSpacing(24),
-              ],
+                  const VerticalSpacing(16),
+                  const Text(
+                    'Please enter the email associated with your account.',
+                    style: TextStyle(color: AppColors.neutral600),
+                  ),
+                  const VerticalSpacing(32),
+                  Form(
+                    key: controller.formKey,
+                    child: AppTextFormField(
+                      controller: controller.emailController,
+                      validator: ValidationBuilder().email().build(),
+                      labelText: 'Email Address',
+                      textInputType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  const VerticalSpacing(32),
+                  AppOutlinedButton(
+                    text: 'Continue',
+                    isLoading: controller.forgotPasswordState.value ==
+                        TheStates.loading,
+                    onPressed: () async {
+                      if (!AppUtils.validateForm(controller.formKey)) {
+                        return;
+                      }
+                      if (await controller.forgotPassword()) {
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                  const VerticalSpacing(24),
+                ],
+              ),
             ),
           ),
         ),
